d59b70f82ec14734735b121c13ab86a19db373c4
Nur88 <momozity24@gmail.com>
2025-09-27 10:22:56 +0800

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-  ],
+  ], base: process.env.NODE_ENV === 'production' ? '/Product-Listing-Page/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
